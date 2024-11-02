ca1ffa053fc949f41523d7d07ca8c25f8e3afbc4
jamesfulreader <jamesfulreader@gmail.com>
2024-11-02 09:11:03 -0600

    basic cli attempt

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+func commandHelp() {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println()
+	fmt.Println("Usage:\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	fmt.Println()
+}
+
+func commandExit() {
+	fmt.Println("Exiting . . .")
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input := scanner.Text()
+
+		if input == "help" {
+			commandHelp()
+		} else if input == "exit" {
+			commandExit()
+		} else {
+			fmt.Println("unrecognized command")
+			commandHelp()
+		}
+	}
+
 }
