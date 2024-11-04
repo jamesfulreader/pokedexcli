162c32fd2c48ff1199935d8aeceb13ccf69064b1
jamesfulreader <jamesfulreader@gmail.com>
2024-11-03 17:00:20 -0700

    feat: GET 20 locations and display in terminal in CommandMap. Give up on writing tests

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,13 +1,8 @@
 package cmd
 
 import (
-	"net/http"
 	"testing"
 )
-
-type MockHTTPClient struct {
-	DoFunc func(*http.Request) (*http.Response, error)
-}
 
 func TestMap(t *testing.T) {
 
--- cmd/map.go
+++ cmd/map.go
@@ -1,8 +1,50 @@
 package cmd
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type LocationURL struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous any    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
 
 func CommandMap() error {
-	fmt.Println("Map function to get 20 locations in the pokemon world")
+
+	res, err := http.Get("https://pokeapi.co/api/v2/location/?offset=0&limit=20")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	locationURL := LocationURL{}
+	err = json.Unmarshal(body, &locationURL)
+	if err != nil {
+		log.Fatalf("error with unmarshaling JSON: %s", err)
+	}
+
+	locationsArray := locationURL.Results
+	for _, locationItem := range locationsArray {
+		fmt.Println(locationItem.Name)
+	}
+
 	return nil
 }
