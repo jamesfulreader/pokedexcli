f1f8cefd4bab0838aef7e75bde4dfeb4538ac2a9
jamesfulreader <jamesfulreader@gmail.com>
2024-11-04 19:31:13 -0700

    feat: map and mapb display 20 locations and if prevURL is null display cannot go back message

--- cmd/map.go
+++ cmd/map.go
@@ -10,7 +10,7 @@ import (
 
 type LocationURL struct {
 	Count    int     `json:"count"`
-	Next     string  `json:"next"`
+	Next     *string `json:"next"`
 	Previous *string `json:"previous"`
 	Results  []struct {
 		Name string `json:"name"`
@@ -18,12 +18,19 @@ type LocationURL struct {
 	} `json:"results"`
 }
 
-// var locationMeta []LocationURL
-// var locationMetadata = make([]any, 0)
+var nextURL string
+var prevURL string
+var count int = 0
+var pokeapi string = "https://pokeapi.co/api/v2/location/?offset=0&limit=20"
 
 func CommandMap() error {
+	if count >= 0 {
+		if nextURL != "" { // Check if nextURL is not nil
+			pokeapi = nextURL
+		}
+	}
 
-	res, err := http.Get("https://pokeapi.co/api/v2/location/?offset=0&limit=20")
+	res, err := http.Get(pokeapi)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,32 +51,63 @@ func CommandMap() error {
 		log.Fatalf("error with unmarshaling JSON: %s", err)
 	}
 
-	// locationMeta = append(locationMeta, locationURL)
-	// locationMetadata = append(locationMetadata, locationURL.Count)
-	// locationMetadata = append(locationMetadata, locationURL.Next)
-	// locationMetadata = append(locationMetadata, locationURL.Previous)
+	if locationURL.Next != nil {
+		nextURL = *locationURL.Next
+	}
+
+	if locationURL.Previous != nil {
+		prevURL = *locationURL.Previous
+	}
 
 	locationsArray := locationURL.Results
 	for _, locationItem := range locationsArray {
 		fmt.Println(locationItem.Name)
 	}
-
+	count++
 	return nil
 }
 
 func CommandMapB() error {
-	// prev := LocationURL{}
-	// err = json.Unmarshal(locationMetadata[0], &prev)
-	// if err != nil {
-	// 	log.Fatalf("error with unmarshaling JSON: %s", err)
-	// }
-	// fmt.Println(locationMetadata[0])
-	// fmt.Println(locationMetadata[1])
-	// fmt.Println(locationMetadata[2])
-	// if locationMetadata == string {
-	// 	fmt.Println("cannot go back")
-	// }
+	if count <= 0 {
+		fmt.Println("cannot go back")
+	}
+	if prevURL != "" { // Check if prevURL is not nil
+		pokeapi = prevURL
+	}
 
-	// fmt.Println(reflect.TypeOf(locationMetadata[0]))
+	res, err := http.Get(pokeapi)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	locationURL := LocationURL{}
+	err = json.Unmarshal(body, &locationURL)
+	if err != nil {
+		log.Fatalf("error with unmarshaling JSON: %s", err)
+	}
+
+	if locationURL.Next != nil {
+		nextURL = *locationURL.Next
+	}
+
+	if locationURL.Previous != nil {
+		prevURL = *locationURL.Previous
+	}
+
+	locationsArray := locationURL.Results
+	for _, locationItem := range locationsArray {
+		fmt.Println(locationItem.Name)
+	}
+	count--
 	return nil
 }
