2d9f260e32ebc80420982def4e888875e66e7b92
jamesfulreader <jamesfulreader@gmail.com>
2024-11-16 10:18:08 -0700

    feat: pokecache use sync - add entry to map use mutex - get entry use read lock - reapLoop use mutex to remove entry safely - NewCache create new cache use concurrency test: Add example key val pair test: reapLoop add item to cache and remove after set time

---
+++ internal/pokecahe/pokecache.go
@@ -0,0 +1,63 @@
+package pokecahe
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mutex    sync.RWMutex
+	interval time.Duration
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	entry, ok := c.entries[key]
+	if !ok {
+		return nil, false
+	}
+
+	return entry.val, true
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.mutex.Lock()
+		for key, entry := range c.entries {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mutex.Unlock()
+	}
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := &Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go cache.reapLoop()
+	return cache
+}
